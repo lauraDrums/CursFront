e6767c7086c587eeb3d537aa61ae95437ecd23c4
Laura <lauratama84@gmail.com>
2022-02-11 13:02:14 +0100

    Hemo començat js

---
+++ JavaScript/prova.js
@@ -0,0 +1,37 @@
+// const d = new Date();
+// const b = new Date();
+// var prova1 = "anna";
+// prova1 = "ey";
+// var p = "hola";
+// var p = "adios";
+// console.log(p);
+// document.getElementById("demo").innerHTML = d;
+// document.getElementById("prova").innerHTML = b;
+// console.log("data d'avui " + d);
+// console.log("Això es una prova")
+// window.alert(d)
+
+// var a = 9;
+// var b = 2;
+
+// console.log("suma =" + (a+b));
+// console.log("resta =" + (a-b));
+// console.log("muliplica =" + (a*b));
+// console.log("divisió =" + (a/b));
+// console.log("modul =" + (a%b));
+
+ var modul = 9%2;
+
+if(modul==1){
+    console.log("true");
+    
+}
+else{
+    console.log("false")
+}
+
+// var a, b, c;
+// console.log("Declaració de variables");
+// console.log("a = "+a+"");
+// console.log("b = "+b+"");
+// console.log("c = "+c+"");
