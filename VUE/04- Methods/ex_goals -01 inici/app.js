88c0eb669fdc241da061b9f80a26c4dfcad74108
Laura <lauratama84@gmail.com>
2022-04-01 12:56:02 +0200

    directives de vue

---
+++ VUE/04- Methods/ex_goals -01 inici/app.js
@@ -0,0 +1,12 @@
+var vm = new Vue({
+    el: "#app",
+    data: {
+        goals: [],
+        enteredValue:""
+    },
+    methods: {
+        addGoal({
+            console.log();
+        })
+    }
+})
